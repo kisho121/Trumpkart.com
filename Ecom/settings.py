6e6513f5cf895283b9697c31917eb24877d23826
kisho121 <kishorehitter1995@gmail.com>
2024-08-24 12:13:18 +0530

    uploads moved

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wz#=vy&18^j$y$zi=#0o%rerfq8q^h@i!4h!qi$lku=p#00p*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Ecom.urls'
@@ -147,15 +148,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
-MEDIA_ROOT = BASE_DIR/'static'
-
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
 
-STATIC_ROOT = 'C:/Users/kishore/OneDrive/Desktop/shopkart/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = BASE_DIR/'media'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
