ba0f2971f9b8efbe4f0dc75915bc1d15b32d2a5e
kisho121 <kishorehitter1995@gmail.com>
2024-08-28 02:42:42 +0530

    update allowed host

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),  # Default to localhost if not set
+        'HOST': os.environ.get('DB_HOST'), 
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'PORT': os.environ.get('DB_PORT', '3306'),
