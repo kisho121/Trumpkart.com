a3562c9a61491c2f6eb877fe4783afd0315fbab2
kisho121 <kishorehitter1995@gmail.com>
2024-08-28 02:19:23 +0530

    update allowed host

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wz#=vy&18^j$y$zi=#0o%rerfq8q^h@i!4h!qi$lku=p#00p*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = ['trumpkart-35.onrender.com', 'www.trumpkart-35.onrender.com']
 
