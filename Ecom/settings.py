9d32eed3f1b68faf23d208d2fe7f6265b4fd61e6
kisho121 <kishorehitter1995@gmail.com>
2024-08-28 16:44:31 +0530

    update debug

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -163,6 +163,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
